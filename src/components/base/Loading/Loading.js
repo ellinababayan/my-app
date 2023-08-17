175970141d6a797102cbe21d7fb1a4696b124402
Ellina Babaian <famogl375@gmail.com>
2023-08-16 23:31:22 -0700

    Loading animation styling

--- src/components/base/Loading/Loading.js
+++ src/components/base/Loading/Loading.js
@@ -1,10 +1,12 @@
 import React from "react";
 import LoadingImg from "./Loading.svg";
+import classes from "./Loading.module.css"
 
 let Loading = (props) => {
   return (
-    <div style={{ backgroundColor: "white"}}>
-      <img src={LoadingImg} />
+    // <div style={{ backgroundColor: "white"}}>
+    <div className={classes.loading}>
+      <img src={LoadingImg} className={classes.loadingImg} />
     </div>
   );
 };
