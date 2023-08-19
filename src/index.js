fd5c0c71bd356ee00bc6756b43f994c8ea376a35
Ellina Babaian <famogl375@gmail.com>
2023-08-19 12:24:25 -0700

    clean up

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 import "./index.css";
-import reportWebVitals from "./reportWebVitals";
+// import reportWebVitals from "./reportWebVitals";
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
@@ -27,5 +27,4 @@ rerenderEntireTree(store.getState());
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
 // store.subscribe(rerenderEntireTree);
